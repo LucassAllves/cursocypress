581d5ff2f2fe674567c57e984b031dc297dbde50
lucas-lcs <la.devtech@gmail.com>
2024-03-23 13:15:34 -0300

    test(upgrade): update version cypress

---
+++ cypress/e2e/basic.cy.js
@@ -0,0 +1,52 @@
+/// <reference types="cypress"/>
+    // Acessando a primeira Url 
+describe ("Cypress basics", () => {
+    it('Should visit a page and assert title', ( ) => {
+         cy.visit('https://www.wcaquino.me/cypress/componentes.html ')// visitamos usando o cy.visit
+         //const title = cy.title() esse nao pega o texto porque não esta assincrono
+        // console.log(title) ou seja a aula de promises. 
+        //cy.pause// serve para vc ir pausando o testes.
+        //.debug// Serve para ter mais detalhe sobre o testes e elementos.
+        cy.title().should('be.equal', 'Campo de Treinamento')// usamos o Should para fazer o cypress encontrar o tempo que for necessario  
+        cy.title().should('contain', 'Campo') 
+       
+       cy.title()
+            .should('be.equal', 'Campo de Treinamento')
+            .and('contain', 'Campo')// essa é uma forma de fazer colocando o and no lugar do should
+            
+            
+            let syncTitle 
+            cy.title().then(title => {// aqui eu posso colocar o should que finciona tbm no lugar do then.
+            console.log(title)
+            
+            cy.get('#formNome').type(title)//vou imprimir o testo que esta em title e colocar ela no campo mapeado.
+          
+          
+            syncTitle = title
+        })//TODO imprimir o log no coonsole.
+       
+        cy.get('#elementosForm\\:sugestoes').then($el => {
+            cy.wrap($el).type(syncTitle)
+
+        })
+       
+    })
+
+    it ("Should find and interact whit an element", () => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html ')
+       
+       
+        
+
+
+        //cy.get('#buttonSimple').click() Essa é um forma de fazer 
+        //cy.get('#buttonSimple').should('have.value', 'Obrigado!')
+        cy.get('#buttonSimple')
+        .click() 
+        .should('have.value', 'Obrigado!')// essa é a sugunda forma nao mapeando o elemento d novo 
+        // lembrando que temos que seta o valor (value) ou o caminha para i cyress
+        
+
+    })
+})
+
---
+++ cypress/e2e/fixture.e.cy.js
@@ -0,0 +1,26 @@
+///<reference types = 'cypress'/>
+
+
+describe('Fixture tests',  () => {   //quando se usa o this as vezes ele recusa o => arrow ai precisamos mudar para o function
+    it('Get data form fixture file ', function () {
+      cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+        
+      cy.fixture('userData').as('usuario').then(() => {
+            cy.get('#formNome').type(this.usuario.nome)
+            cy.get('#formSobrenome').type(this.usuario.sobrenome)
+            cy.get(`[name=formSexo][value=${this.usuario.sexo}]`).click()  //temos que pegar nome e value para passar o parametro da fixture
+           
+            cy.get(`[name=formComidaFavorita][value=${this.usuario.comida}]`).click()
+           
+            cy.get('#formEscolaridade').select(this.usuario.escolaridade)
+            cy.get('#formEsportes').select(this.usuario.esportes)
+            cy.get('#formCadastrar').click()
+            cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')// Pegamos o arquivo que esta em fixture e usamos aqui para preencher os valores
+        })
+           
+     
+    })
+    
+
+
+})
